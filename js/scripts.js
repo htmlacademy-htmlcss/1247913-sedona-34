212b216fbcb477af875aa74c33075132a5c6c80d
Yan Shatskiy <60981097+jan-web@users.noreply.github.com>
2022-07-12 17:23:24 +0200

    feat: Final countdown

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,18 @@
 const modalSearchCloseButton = document.querySelector('.modal-search__close-button');
 const modal = document.querySelector('.modal');
 const buttonSearch = document.querySelector('.button_search');
+const bodyElem = document.querySelector('body');
 const toggleModal = () => {
    modal.classList.toggle('modal_open');
+   bodyElem.classList.toggle('overflow-hidden');
 }
 
-modalSearchCloseButton.addEventListener('click', toggleModal)
-buttonSearch.addEventListener('click', (e)=>{
-  console.log(e);
-  e.preventDefault();
-  toggleModal();
+if(modalSearchCloseButton) {
+  modalSearchCloseButton.addEventListener('click', toggleModal)
+  buttonSearch.addEventListener('click', (e)=>{
+    console.log(e);
+    e.preventDefault();
+    toggleModal();
+  })
+}
 
-})
